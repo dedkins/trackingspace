bb6c255168bd361e5e508d72208859e873fc166f
David Edkins <dedkins@edkinsgroup.com>
2012-05-22 09:05:24 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackingspace::Application.config.secret_token = 'e5fcea1d876cda7c7e5e078fdf25a5e56d2948e4d7f697ab9237db61d37ba257ccfc68a220efc0a5ab505580d740cf6cec827372a5f616d6c8caee3bcbbedcaf'
