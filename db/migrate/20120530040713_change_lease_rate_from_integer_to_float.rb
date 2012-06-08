3a553ed3b22556b53fe4ce7f6d57f13dfda95c6d
David Edkins <dedkins@edkinsgroup.com>
2012-06-08 06:12:33 -0700

    changes

--- db/migrate/20120530040713_change_lease_rate_from_integer_to_float.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLeaseRateFromIntegerToFloat < ActiveRecord::Migration
-  def up
-    change_column :leases, :current_rate, :float
-  end
-
-  def down
-    change_column :leases, :current_rate, :integer
-  end
-end
--- db/migrate/20120530050642_change_rate_from_money_to_decimal.rb
+++
@@ -1,9 +0,0 @@
-class ChangeRateFromMoneyToDecimal < ActiveRecord::Migration
-  def up
-    change_column :leases, :current_rate, :decimal
-  end
-
-  def down
-    change_column :leases, :current_rate, :money
-  end
-end
