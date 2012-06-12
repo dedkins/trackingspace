c01e8b14cdf5e8eb97328e71e5556fa99d9b9288
David Edkins <dedkins@edkinsgroup.com>
2012-06-12 08:39:35 -0700

    update product.rb to include catchall for css js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Trackingspace::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Catchall for css and js files
+  config.assets.precompile += %w( *.css *.js )
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
