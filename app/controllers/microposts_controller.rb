7dca3c4e2874662006b95cad20f6cb977d9178bb
David Edkins <dedkins@edkinsgroup.com>
2012-07-06 16:06:24 -0700

    made some visual and functional changes for feeds, maps

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@ class MicropostsController < ApplicationController
 
 def create
 	@micropost = current_user.microposts.build(params[:micropost])
+
 	if @micropost.save
 		flash[:success] = "Micropost Saved"
 		redirect_to current_user
