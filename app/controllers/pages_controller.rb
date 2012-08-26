28138254933fd915b0f48473274bef43b2307452
David Edkins <dedkins@edkinsgroup.com>
2012-08-26 15:48:07 -0700

    added pages controller through rails g process

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
 
-def home
-	@newbuildings = Building.new_buildings
-	@json = Building.new_buildings.to_gmaps4rails
-end
+	def home
+		@newbuildings = Building.new_buildings
+		@json = Building.new_buildings.to_gmaps4rails
+	end
 
 end
