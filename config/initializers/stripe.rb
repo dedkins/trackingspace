dce77e7c7a57f1d0b5993c40016b36b847b14e70
David Edkins <dedkins@edkinsgroup.com>
2012-10-30 09:01:01 -0700

    added exception to subscription and updated stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,6 +2,6 @@ if Rails.env == "development"
 	Stripe.api_key = "sk_190fdn9FJBeZHZ5vxE3mQpzVvA14d"
 	STRIPE_PUBLIC_KEY = "pk_3DoGLoNWEMh4o6Y5tCNuxVbC7lbRg"
 elsif Rails.env == "production"
-	Stripe.api_key = "sk_190fdn9FJBeZHZ5vxE3mQpzVvA14d"
-	STRIPE_PUBLIC_KEY = "pk_3DoGLoNWEMh4o6Y5tCNuxVbC7lbRg"
+	Stripe.api_key = "sk_CnitY8ki4HtuE0Kv4eIfDN2vjQsFR"
+	STRIPE_PUBLIC_KEY = "pk_x0QbYj8w3PdUEYN6IHiNw4RZFfnxX"
 end
