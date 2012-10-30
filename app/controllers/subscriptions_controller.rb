afc91507dd17d057d6cedb4332f4c243f8755b56
David Edkins <dedkins@edkinsgroup.com>
2012-10-30 08:58:43 -0700

    added exception to subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,17 +4,21 @@ class SubscriptionsController < ApplicationController
 	end
 
 	def create
-	@user = User.find(current_user.id)
-	@subscription = Subscription.new(params[:subscription])
-		if @subscription.save_with_payment
-			@user.upgrade = 'Upgrade59'
-			@user.save!
-			UserMailer.upgrade_email(@user).deliver
-			UserMailer.upgrade_notification(@user).deliver
-		redirect_to @user, :notice => "Thank you for 
-		  subscribing!"
-		else
-		render :new
+		begin
+			@user = User.find(current_user.id)
+			@subscription = Subscription.new(params[:subscription])
+				if @subscription.save_with_payment
+					@user.upgrade = 'Upgrade59'
+					@user.save!
+					UserMailer.upgrade_email(@user).deliver
+					UserMailer.upgrade_notification(@user).deliver
+				redirect_to @user, :notice => "Thank you for 
+				  subscribing!"
+				else
+				render :new
+				end
+		rescue Exception => e
+			redirect_to upgrade_path
 		end
 	end
 
