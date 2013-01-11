a2c2cb3ae619d89303bebe890f533af71568f896
David Edkins <dedkins@edkinsgroup.com>
2013-01-10 20:57:50 -0800

    building redirect fix for untrack

--- app/controllers/building_relationships_controller.rb
+++ app/controllers/building_relationships_controller.rb
@@ -21,7 +21,7 @@ class BuildingRelationshipsController < ApplicationController
 		end
 		current_user.building_relationships.find(@br).destroy
 		respond_to do |format|
-			format.html { redirect_to building_path(@building) }
+			format.html { redirect_to building_path(@br.building_id) }
 			format.js
 		end
 	end
