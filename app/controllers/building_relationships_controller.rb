e3a2ff6f0ba865c2f58a1d197ba765ed945a792a
David Edkins <dedkins@edkinsgroup.com>
2013-01-12 10:56:58 -0800

    cmon will this work

--- app/controllers/building_relationships_controller.rb
+++ app/controllers/building_relationships_controller.rb
@@ -14,7 +14,7 @@ class BuildingRelationshipsController < ApplicationController
 		@user = current_user
 		@br = BuildingRelationship.find(params[:id])
 		@building = Building.find(params[:building_id])
-		current_user.building_relationships.find(@br.id).destroy
+		current_user.building_relationships.find_by_building_id(@building.id).destroy
 		respond_to do |format|
 			format.html { redirect_to @building.id }
 			format.js
