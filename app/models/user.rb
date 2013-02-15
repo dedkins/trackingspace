a40c8f6dc58b651bf44b0e0b177fde17efeacedf
David Edkins <dedkins@edkinsgroup.com>
2013-02-15 12:23:11 -0800

    removed acts as gmappable

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,6 @@ class User < ActiveRecord::Base
   end
   after_validation :reverse_geocode
 
-  acts_as_gmappable
   
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
