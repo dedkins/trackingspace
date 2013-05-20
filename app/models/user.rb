d9bafa5146a76e14f6b756f5d13ac2b8099511cf
David Edkins <dedkins@edkinsgroup.com>
2013-05-20 11:14:04 -0700

    removing order from shared lease list

--- app/models/user.rb
+++ app/models/user.rb
@@ -108,7 +108,7 @@ class User < ActiveRecord::Base
   def leases
     @shared = LeaseShare.where("sharedto_id = ? OR email = ?", id, email).all
     unless @shared.empty?
-      ((Lease.where("user_id = ?", id).all + Lease.find(@shared.map(&:lease_id).uniq)).uniq).order('expiration ASC')
+      (Lease.where("user_id = ?", id).all + Lease.find(@shared.map(&:lease_id).uniq)).uniq
     else
       Lease.where("user_id = ?", id).order('expiration ASC')
     end
