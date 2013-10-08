921b2e72e55993b8755e2f6b5c76e308b7ff1800
David Edkins <dedkins@edkinsgroup.com>
2013-10-08 15:07:15 -0700

    updated config secret for devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  config.secret_key = 'some1234keyq23' if Rails.env == 'production'
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
